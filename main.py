88b02d8d9704233d817e3601774bed762a1732aa
Pamal Mangat <mangatpamal@gmail.com>
2020-07-25 16:09:27 -0700

    - Was able to display the contents of the array and now implemented a method that allows the user to add elements in at a given index. - Next i need to implement a method that allows for searching and deleting of elements.
    Signed-off-by: pamalmangat <pamal13@hotmail.com>

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class VisualArray:
         # Numpy array attributes.
         self.dataType = None
         self.numDimensions = None
+        self.array = []
+
+        self.beta = []
 
         # Check to see if user dimension entry is valid.
         def valid_Dimension():
@@ -138,14 +141,126 @@ class VisualArray:
 
     # Method displays the array alongside the methods and tasks that can be performed on the array. 
     def arrayHub(self):
-        print('Array # Dimensions: ' + str(self.get_NumDimensions()))
-        print('Array Data Type: ' + self.get_DataType())
 
         # Beta window displays the list's contents and buttons to respective methods that can be performed on array.
-        beta = tk.Tk()
-        beta.title('Visual Array Hub')
-        beta.geometry('875x525')
-        beta.mainloop()
+        self.beta = tk.Tk()
+
+        # Method destroys current GUI and restarts application.
+        def restart():
+            self.beta.destroy()
+            self.beta.quit()
+            self.__init__()
+
+        # Frame to listbox containing array elements.
+        arrayFrame = tk.Frame(self.beta, bg='indianred')
+
+        label1 = tk.Label(arrayFrame, text='Array Contents:', fg='white', bg='black', font='Helvetica 24 bold')
+        label1.pack(fill=tk.X, pady=(10,30))
+
+        # Listbox to hold the contents of the array.
+        listBox = tk.Listbox(arrayFrame, justify='center', font='TIMES 26 bold', width=20, height=10,
+                             selectborderwidth=1, bg='lavenderblush', relief='sunken', selectbackground='oldlace')
+
+        for key in self.array:
+            listBox.insert(tk.END, key)
+
+        listBox.pack(side=tk.RIGHT)
+
+        # If the content of the array exceeds 11, then insert a scroll bar left of the listbox.
+        if listBox.size() >= 11:
+            # Add scrollbar to listbox.
+            scrollbar = tk.Scrollbar(arrayFrame, orient="vertical", command=listBox.yview)
+            scrollbar.pack(side=tk.LEFT, fill=tk.Y)
+            listBox.config(yscrollcommand=scrollbar.set)
+
+        arrayFrame.grid(row=0, column=0)
+        self.beta.columnconfigure(0, weight=1)
+        self.beta.rowconfigure(0, weight=1)
+
+        # Frame containing buttons with methods to perform on the given array.
+        methodFrame = tk.Frame(self.beta, bg='indianred')
+
+        label3 = tk.Label(methodFrame, text='Array Methods:', fg='white', bg='black', font='Helvetica 24 bold')
+        label3.pack(fill=tk.X, pady=10)
+
+        insertButton = tk.Button(methodFrame, text='INSERT', font='HELVETICA 30 bold', width=20, command=lambda: self.insert())
+        insertButton.pack(fill=tk.X, pady=10)
+
+        deleteButton = tk.Button(methodFrame, text='DELETE', font='HELVETICA 30 bold', width=20)
+        deleteButton.pack(fill=tk.X, pady=10)
+
+        searchButton = tk.Button(methodFrame, text='SEARCH', font='HELVETICA 30 bold', width=20)
+        searchButton.pack(fill=tk.X, pady=10)
+
+        splitButton = tk.Button(methodFrame, text='SPLIT', font='HELVETICA 30 bold', width=20)
+        splitButton.pack(fill=tk.X, pady=10)
+
+        sortButton = tk.Button(methodFrame, text='SORT', font='HELVETICA 30 bold', width=20)
+        sortButton.pack(fill=tk.X, pady=10)
+
+        filterButton = tk.Button(methodFrame, text='FILTER', font='HELVETICA 30 bold', width=20)
+        filterButton.pack(fill=tk.X, pady=10)
+
+        methodFrame.grid(row=0, column=1)
+        self.beta.columnconfigure(1, weight=1)
+        self.beta.rowconfigure(1, weight=1)
+
+        label4 = tk.Label(self.beta, text='Number of Dimensions: ' + str(self.get_NumDimensions()) + '\nType: ' +
+                          str(self.get_DataType()) + '\n[' + str(listBox.size()) + '] elements.', bg='indianred',
+                          font='HELVETICA 18')
+        label4.grid(row=1, column=0)
+
+        newArrayButton = tk.Button(self.beta, text='NEW ARRAY', font='HELVETICA 30 bold', width=20,
+                                   command=lambda: restart())
+        newArrayButton.grid(row=1, column=1)
+
+        # Beta window attributes.
+        self.beta.config(bg='indianred')
+        self.beta.title('Visual Array Hub')
+        self.beta.geometry('875x525')
+        self.beta.minsize(875, 525)
+        self.beta.mainloop()
+
+    # Method inserts element at given location in array.
+    def insert(self):
+
+        def add():
+            self.array.insert(int(indexEntry.get()), elementEntry.get())
+            print('Inserted [' + str(elementEntry.get()) + '] @ Index ' + str(indexEntry.get()))
+            charlie.destroy()
+            charlie.quit()
+            self.beta.destroy()
+            self.beta.quit()
+            self.arrayHub()
+
+
+        charlie = tk.Tk()
+
+        label1 = tk.Label(charlie, text='Insert Element:', bg='gray28', fg='white', font='HELVETICA 20 bold')
+        label1.pack(fill=tk.X, padx=20, pady=20)
+
+        # String variables to hold given array and element entered by user.
+        element = tk.StringVar()
+        index = tk.StringVar()
+
+        elementEntry = tk.Entry(charlie, font='HELVETICA 24 bold', justify='center', textvariable=element)
+        elementEntry.pack(fill=tk.X, padx=20)
+
+        label2 = tk.Label(charlie, text='@ Index: ', bg='gray28', fg='white', font='HELVETICA 20 bold',
+                          textvariable=index)
+        label2.pack(fill=tk.X, padx=20, pady=20)
+
+        indexEntry = tk.Entry(charlie, font='HELVETICA 24 bold', justify='center')
+        indexEntry.pack(fill=tk.X, padx=20)
+
+        insertButton = tk.Button(charlie, text='INSERT', font='HELVETICA 24 bold', command=lambda: add())
+        insertButton.pack(fill=tk.X, padx=20, pady=20)
+
+        charlie.geometry('400x300')
+        charlie.title('INSERT:')
+        charlie.config(bg='indianred')
+        charlie.minsize(400, 300)
+        charlie.mainloop()
 
     # Getter methods.
     def get_NumDimensions(self):
@@ -153,6 +268,9 @@ class VisualArray:
 
     def get_DataType(self):
         return self.dataType
+
+    def get_Array(self):
+        return self.array
 
     # Setter methods.
     def set_NumDimensions(self, val):
